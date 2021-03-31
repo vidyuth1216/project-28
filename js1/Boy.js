c255899e9c21cc0922331b94863d02480d0ef8e6
vidyuth1216 <75615784+vidyuth1216@users.noreply.github.com>
2021-03-31 12:19:30 +0530

    Add files via upload

---
+++ js1/Boy.js
@@ -0,0 +1,26 @@
+class Boy {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        };
+        this.pointB = pointB;
+        this.boy = Constraint.create(options);
+        World.add(world, this.boy);
+    }
+
+    fly() {
+        this.boy.bodyA = null;
+    }
+
+    display() {
+        if (this.boy.bodyA) {
+            var pointA = this.boy.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+}
